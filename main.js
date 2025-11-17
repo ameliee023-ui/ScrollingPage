a9cf6787db119cefc63af7cf3d842d6e1501ae47
ameliee023-ui <ameliee023@gmail.com>
2025-11-17 13:06:23 +0100

    changes

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 console.clear();
-
 gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener('load', () => {
-    gsap.timeline({
-        scrollTrigger: {
-            trigger: '.wrapper',
-            start: 'top top',
-            end: '+=200%',
-            pin: true,
-            scrub: true,
-            markers: true
-        }
-    })
-    .to('.tunnel img', {
-        scale: 2,
-        z: 500,
-        rotationY: 10,
-        transformOrigin: 'center center',
-        ease: 'power1.inOut'
-    })
-    .to('.section.hero', {
-        scale: 1.05,
-        transformOrigin: 'center center',
-        ease: 'power1.inOut'
-    }, '<');
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: '.wrapper',
+      start: 'top top',
+      end: '+=200%',
+      pin: true,
+      scrub: true,
+      markers: false
+    }
+  })
+  .to('.tunnel img', {
+    scale: 2,
+    z: 500,
+    rotationY: 10,
+    transformOrigin: 'center center',
+    ease: 'power1.inOut'
+  })
+  .to('.section.hero', {
+    scale: 1.05,
+    transformOrigin: 'center center',
+    ease: 'power1.inOut'
+  }, '<')
+  .to('.hero .title', {
+    scale: 3.5,
+    opacity: 1,
+    z: 300,
+    ease: 'power1.inOut'
+  }, '<');
 });
